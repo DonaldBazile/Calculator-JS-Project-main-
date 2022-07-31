d5a5e0fd7aeaa3c15d0264a74750cc9a8e64f07e
Donbazile07 <donbazile007@gmail.com>
2022-07-31 18:51:59 -0400

    Update

--- public/script.js
+++ public/script.js
@@ -70,7 +70,7 @@ for(let i =0;i<operator.length;i++){
 let number = document.getElementsByClassName("number");
 for(let i =0;i<number.length;i++){
 	number[i].addEventListener('click',function(){
-		var output=reverseNumberFormat(getOutput());
+		let output=reverseNumberFormat(getOutput());
 		if(output!=NaN){
 			output=output+this.id;
 			printOutput(output);
